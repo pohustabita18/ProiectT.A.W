a2ff7446a08929f0d0321337fc41cfc31cf8da8a
Tabita Pohus <emanuela.pohus@emanuel.ro>
2024-11-20 22:06:01 +0200

    adaugare fisiere/pagini

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
    base: '',
    build:{
     rollupOptions: {
-      input: [ "./index.html", "./aboutUs.html","./products.html","./event.html","./contact.html","./shop.html","./faq.html","./cart.html","./checkout.html","./product.html","./recipe.html"]
+      input: [ "./index.html", "./aboutUs.html","./event.html","./contact.html","./shop.html","./faq.html","./cart.html","./checkout.html","./product.html","./recipe.html"]
     }
   }
 })
